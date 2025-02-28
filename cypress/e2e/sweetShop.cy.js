92f07bddca11087a4c2b0582b7faff258d9ba95b
Vaidul1s <vaidas.ad25@gmail.com>
2025-02-28 08:54:23 +0200

    02.28

--- cypress/e2e/sweetShop.cy.js
+++ cypress/e2e/sweetShop.cy.js
@@ -4,5 +4,17 @@ describe('Sweet Shop', () => {
     cy.visit('https://sweetshop.netlify.app/')
   });
 
+  it('Login', () => {
+    cy.visit('https://sweetshop.netlify.app/');
+    cy.get('.nav-link').contains('Login').click();
+
+    cy.get('#exampleInputEmail').click().type('test@example.com');
+    cy.get('#exampleInputPassword').click().type('1234');
+
+    cy.get('button[type="submit"]').click();
+
+    cy.contains('Welcome back test@user.com').should('be.visible');
+  });
+
 
 });
