d7a6647b9526c7070a46b846119331307baf06bc
Vaidul1s <vaidas.ad25@gmail.com>
2025-03-06 09:49:24 +0200

    03.06

--- cypress/e2e/products.cy.js
+++ cypress/e2e/products.cy.js
@@ -8,10 +8,33 @@ describe('Products', () => {
         cy.visit('https://sweetshop.netlify.app/sweets');
     });
 
-    it('2. Procuts have images', () => {
-        cy.get('.card').each(($img) =>{
-            cy.wrap($img).should('have.members','img.card-img-top');
-        })
+    it('2. Products have images', () => {
+        cy.get('.card .card-img-top').each(($el) => {
+            cy.wrap($el).should('be.visible');
+        });
     });
 
+    it('3. Products have names', () => {
+        cy.get('.card .card-title').each(($el) => {
+            cy.wrap($el).should('be.visible').and('not.be.empty');
+        });
+    });
+
+    it('4. Products have text', () => {
+        cy.get('.card .card-text').each(($el) => {
+            cy.wrap($el).should('be.visible').and('not.be.empty');
+        });
+    });
+
+    it('5. Products have prices', () => {
+        cy.get('.card .text-muted').each(($el) => {
+            cy.wrap($el).should('be.visible').and('not.be.empty');
+        });
+    });
+
+    it('6. Products have add to basket button', () => {
+        cy.get('.card .addItem').each(($el) => {
+            cy.wrap($el).should('be.visible');
+        });
+    });
 });
