9dc80d6896807a379589dcc84420e1bfe2b27b90
Vaidul1s <vaidas.ad25@gmail.com>
2025-03-07 08:42:15 +0200

    03.07

--- cypress/e2e/home_and_nav.cy.js
+++ cypress/e2e/home_and_nav.cy.js
@@ -1,6 +1,6 @@
 describe('Sweet Shop Home Page and Navigation', () => {
 
-    it('Home Page', () => {
+    it('1. Home Page', () => {
       cy.visit('https://sweetshop.netlify.app/');
       cy.get('.display-3').contains('Welcome to the sweet shop!').should('be.visible');
       cy.get('nav.navbar').should('be.visible');
@@ -10,24 +10,29 @@ describe('Sweet Shop Home Page and Navigation', () => {
         cy.visit('https://sweetshop.netlify.app/');
     });
 
-    it('Navigation routes Sweets', () => {
+    it('2. Navigation route - Sweets', () => {
         cy.get('.nav-link').contains('Sweets').click();
         cy.url().should('eq', 'https://sweetshop.netlify.app/sweets');
     });
   
-    it('Navigation routes About', () => {
+    it('3. Navigation route - About', () => {
         cy.get('.nav-link').contains('About').click();
         cy.url().should('eq', 'https://sweetshop.netlify.app/about');
     });
 
-    it('Navigation routes Login', () => {
+    it('4. Navigation route - Login', () => {
         cy.get('.nav-link').contains('Login').click();
         cy.url().should('eq', 'https://sweetshop.netlify.app/login');
     });
 
-    it('Navigation routes Basket', () => {
+    it('5. Navigation route - Basket', () => {
         cy.get('.nav-link').contains('Basket').click();
         cy.url().should('eq', 'https://sweetshop.netlify.app/basket');
+    });
+
+    it('6. Navigation route - Sweet Shop', () => {
+        cy.get('.navbar-brand').contains('Sweet Shop').click();
+        cy.url().should('eq', 'https://sweetshop.netlify.app/');
     });
     
   
--- cypress/e2e/payment.cy.js
+++ cypress/e2e/payment.cy.js
@@ -1,3 +1,44 @@
 describe('Checkout and Payment', () => {
+    it('1. Filling checkout form successfully', () => {
+        cy.visit('https://sweetshop.netlify.app/');
+        cy.get('.nav-link').contains('Basket').click();
+        cy.url().should('eq', 'https://sweetshop.netlify.app/basket');
+
+        cy.get('.col-md-6 input').eq(0).click().type('Bebras');
+        cy.get('.col-md-6 input').eq(1).click().type('Babrauskas');
+        cy.get('#email').click().type('bebras@example.com');
+        cy.get('#address').click().type('Rastu 11');
+        cy.get('#address2').click().type('Bebriskes');
+        cy.get('#country').select('United Kingdom');
+        cy.get('#city').select('Bristol');
+        cy.get('#zip').click().type('123456');
+        cy.get('#cc-name').click().type('123456');
+        cy.get('#cc-number').click().type('123456');
+        cy.get('#cc-expiration').click().type('123456');
+        cy.get('#cc-cvv').click().type('123456');
+
+        cy.get('button').contains('Continue to checkout').click();
+    });
+
+    it('2. Filling checkout form unsuccessfully', () => {
+        cy.visit('https://sweetshop.netlify.app/');
+        cy.get('.nav-link').contains('Basket').click();
+        cy.url().should('eq', 'https://sweetshop.netlify.app/basket');
+     
+        cy.get('button').contains('Continue to checkout').click();
+
+        cy.get('.invalid-feedback').eq(1).contains('Valid first name is required.').should('be.visible');
+        cy.get('.invalid-feedback').eq(2).contains('Valid last name is required.').should('be.visible');
+        cy.get('.invalid-feedback').eq(3).contains('Please enter a valid email address for shipping updates.').should('be.visible');
+        cy.get('.invalid-feedback').eq(4).contains('Please enter your shipping address.').should('be.visible');
+        cy.get('.invalid-feedback').eq(5).contains('Please select a valid country.').should('be.visible');
+        cy.get('.invalid-feedback').eq(6).contains('Please provide a valid state.').should('be.visible');
+        cy.get('.invalid-feedback').eq(7).contains('Zip code required.').should('be.visible');
+        cy.get('.invalid-feedback').eq(8).contains('Name on card is required').should('be.visible');
+        cy.get('.invalid-feedback').eq(9).contains('Credit card number is required').should('be.visible');
+        cy.get('.invalid-feedback').eq(10).contains('Expiration date required').should('be.visible');
+        cy.get('.invalid-feedback').eq(11).contains('Security code required').should('be.visible');
+        
+    });
 
 });
